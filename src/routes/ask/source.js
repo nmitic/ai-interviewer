72c4a96793c9dbc68a7793a08ccf6e2ec01d1be1
Nikola Mitic <nikola.mitic.dev@gmail.com>
2024-02-10 10:52:35 +0100

    adds more information to the source

--- src/routes/ask/source.js
+++ src/routes/ask/source.js
@@ -23,6 +23,17 @@ const jobsQuery = gql`
       date_when_you_ended_working_here: endDate
       tools_programming_languages_frameworks_you_used_in_this_job: techStackTools
       job_position_title: title
+      project_that_I_worked_on:jobProjects {
+        ... on Post {
+          id
+          title
+          content {
+            project_description:text
+          }
+        }
+      }
+      team_member_structure_and_amount:teamMembersJobTitles
+      projectManagement
     }
     nikola_mitic_thoughts_on_various_subjects: tinyThoughts(first: 100) {
       content {
